a91e82e45acff79fd3dd064223884877954b88fd
tommytyc <tommytyc.cs06@nctu.edu.tw>
2020-04-17 03:06:58 +0800

    finish with the read function

--- HW2/server.py
+++ HW2/server.py
@@ -5,6 +5,8 @@ import socket
 import sys
 import os
 import sqlite3
+import re
+from datetime import date
 
 global HOST
 global PORT
@@ -41,7 +43,7 @@ def Init():
 			)
 		"""
 	)
-	# Create table post(id, board_name, title, content, author)
+	# Create table post(id, board_name, title, content, author, date)
 	cursor.execute(
 		"""
 			create table if not exists post(
@@ -49,7 +51,8 @@ def Init():
 				board_name text,
 				title text,
 				content text,
-				author text
+				author text,
+				date text
 			)
 		"""
 	)
@@ -63,7 +66,9 @@ def Init():
 			)
 		"""
 	)
-	
+
+def Write(conn, msg):
+	conn.sendall(msg.encode())	
 
 def HandleClient():
 	while True:
@@ -102,7 +107,31 @@ def CreateBoard(board_name, moderator):
 	cursor.execute("insert into board(board_name, moderator) values(?, ?)", (board_name, moderator))
 	dbconn.commit()
 
-def HandleCommand(conn, cmd, login_status, login_user):
+def GetBoardList():
+	cursor.execute("select * from board")
+	return cursor.fetchall()
+
+def CreatePost(board_name, title, content, author, date):
+	cursor.execute(" insert into post(board_name, title, content, author, date) values(?, ?, ? ,?, ?) ", (board_name, title, content, author, date))
+	dbconn.commit()
+
+def GetPostList(board_name):
+	cursor.execute(" select id, title, author, date from post where board_name = ? ", (board_name,))
+	return cursor.fetchall()
+
+def GetPost(post_id):
+	cursor.execute(" select author, title, date, content from post where id = ? ", (post_id,))
+	return cursor.fetchone()
+
+def CheckPostExist(post_id):
+	cursor.execute(" select * from post where id = ? ", (post_id,))
+	tmp = cursor.fetchall()
+	if tmp != []:
+		return True
+	else:
+		return False
+
+def HandleCommand(conn, cmd, cmd_orig, login_status, login_user):
 	msg = None
 	if cmd[0] == 'register':
 		if len(cmd) != 4:
@@ -113,7 +142,7 @@ def HandleCommand(conn, cmd, login_status, login_user):
 			else:
 				CreateUser(cmd[1], cmd[2], cmd[3])
 				msg = 'Register successfully.\n'
-		conn.sendall(msg.encode())
+		Write(conn, msg)
 		return login_status, login_user, False
 
 	elif cmd[0] == 'login':
@@ -134,7 +163,7 @@ def HandleCommand(conn, cmd, login_status, login_user):
 						msg = 'Login failed.\n'
 				else:
 					msg = 'Login failed.\n'
-		conn.sendall(msg.encode())
+		Write(conn, msg)
 		return login_status, login_user, False
 		
 	elif cmd[0] == 'whoami':
@@ -145,7 +174,7 @@ def HandleCommand(conn, cmd, login_status, login_user):
 				msg = 'Please login first.\n'
 			else:
 				msg = login_user + '\n'
-		conn.sendall(msg.encode())
+		Write(conn, msg)
 		return login_status, login_user, False
 
 	elif cmd[0] == 'logout':
@@ -158,7 +187,7 @@ def HandleCommand(conn, cmd, login_status, login_user):
 				msg = 'Bye, ' + login_user + '.\n'
 				login_user = None
 				login_status = False
-		conn.sendall(msg.encode())
+		Write(conn, msg)
 		return login_status, login_user, False
 
 	elif cmd[0] == 'create-board':
@@ -173,8 +202,95 @@ def HandleCommand(conn, cmd, login_status, login_user):
 				else:
 					CreateBoard(cmd[1], login_user)
 					msg = 'Create board successfully.\n'
-		conn.sendall(msg.encode())
+		Write(conn, msg)
 		return login_status, login_user, False
+
+	elif cmd[0] == 'list-board':
+		if len(cmd) > 2 or (len(cmd) == 2 and cmd[1][:2] != '##'):
+			msg = 'Usage: list-board ##<key>\n'
+		elif len(cmd) == 1:
+			board_list = GetBoardList()
+			msg = 'Index\t\t\tName\t\t\tModerator\n'
+			board_cnt = 0
+			for board in board_list:
+				board_cnt += 1
+				msg += str(board_cnt) + '\t\t\t' + board[0] + '\t\t\t' + board[1] + '\n'
+
+		elif len(cmd) == 2:
+			board_list = GetBoardList()
+			msg = 'Index\t\t\tName\t\t\tModerator\n'
+			board_cnt = 0
+			key = cmd[1][2:]
+			for board in board_list:
+				if re.search(key, board[0]) != None:
+					board_cnt += 1
+					msg += str(board_cnt) + '\t\t\t' + board[0] + '\t\t\t' + board[1] + '\n'
+
+		Write(conn, msg)
+		return login_status, login_user, False
+
+	elif cmd[0] == 'create-post':
+		if login_status == False:
+			msg = 'Please login first.\n'
+		else:
+			cmd_orig_title = cmd_orig.find('--title')
+			cmd_orig_content = cmd_orig.find('--content')
+			if '--title' in cmd and '--content' in cmd and cmd.index('--title') < cmd.index('--content') and cmd[2] == '--title':
+				if login_status == False:
+					msg = 'Please login first.\n'
+				else:
+					if CheckBoardExist(cmd[1]):
+						today = str(date.today())
+						CreatePost(cmd[1], cmd_orig[cmd_orig_title + 8 : cmd_orig_content], cmd_orig[cmd_orig_content + 10 :], login_user, today)
+						msg = 'Create post successfully.\n'
+					else:
+						msg = 'Board is not exist.\n'
+			else:
+				msg = 'Usage: create-post <board-name> --title <title> --content <content>\n'
+		Write(conn, msg)
+		return login_status, login_user, False
+
+	elif cmd[0] == 'list-post':
+		if len(cmd) > 3 or len(cmd) < 2 or (len(cmd) == 3 and cmd[2][:2] != '##'):
+			msg = 'Usage: list-post <board-name> ##<key>\n'
+		elif len(cmd) == 2:
+			if CheckBoardExist(cmd[1]):
+				msg = 'ID\t\t\tTitle\t\t\tAuthor\t\t\tDate\n'
+				post_list = GetPostList(cmd[1])
+				for post in post_list:
+					day = post[3].split('-', -1)
+					msg += str(post[0]) + '\t\t\t' + post[1] + '\t\t\t' + post[2] + '\t\t\t' + day[1] + '/' + day[2] + '\n'
+			else:
+				msg = 'Board is not exist.\n'
+		elif len(cmd) == 3:
+			if CheckBoardExist(cmd[1]):
+				msg = 'ID\t\t\tTitle\t\t\tAuthor\t\t\tDate\n'
+				post_list = GetPostList(cmd[1])
+				key = cmd[2][2:]
+				for post in post_list:
+					if re.search(key, post[1]) != None:
+						day = post[3].split('-', -1)
+						msg += str(post[0]) + '\t\t\t' + post[1] + '\t\t\t' + post[2] + '\t\t\t' + day[1] + '/' + day[2] + '\n'
+			else:
+				msg = 'Board is not exist.\n'
+		Write(conn, msg)
+		return login_status, login_user, False
+
+	elif cmd[0] == 'read':
+		if len(cmd) != 2:
+			msg = 'Usage: read <post-id>\n'
+		else:
+			post_id = int(cmd[1])
+			if CheckPostExist(post_id):
+				post = GetPost(post_id)
+				post_content = str(post[3]).replace('<br>', '\n')
+				msg = 'Author\t:'
+				msg += post[0] + '\nTitle\t:' + post[1] + '\nDate\t:' + post[2] + '\n--\n' + post_content + '\n--\n'
+			else:
+				msg = 'Post is not exist.\n'
+		Write(conn, msg)
+		return login_status, login_user, False
+
 
 	elif cmd[0] == 'exit':
 		conn.close()
@@ -183,7 +299,7 @@ def HandleCommand(conn, cmd, login_status, login_user):
 
 	else:
 		msg = 'Command not found. Your command is ' + cmd[0] + '.\n'
-		conn.sendall(msg.encode())
+		Write(conn, msg)
 		return login_status, login_user, False
 
 def HandleBBS(conn):
@@ -200,9 +316,9 @@ def HandleBBS(conn):
 			conn.close()
 			conn_pool.remove(conn)
 			break
-		cmd = str(data.decode().strip())
-		cmd = str(cmd).split(' ', -1)
-		login_status, login_user, exitornot = HandleCommand(conn, cmd, login_status, login_user)
+		cmd_orig = str(data.decode().strip())
+		cmd = str(cmd_orig).split(' ', -1)
+		login_status, login_user, exitornot = HandleCommand(conn, cmd, cmd_orig, login_status, login_user)
 		if exitornot:
 			break
 
